8e4cef33a953b617aa4b6e980058a061e52cbe9f
Xavier Perez <duosrx@gmail.com>
2012-02-06 15:10:08 +0100

    Add some comments

--- src/clojure_geeklist/core.clj
+++ src/clojure_geeklist/core.clj
@@ -44,45 +44,61 @@
                  :as :json))))
 
 (defn user
+  "Get the authenticated user or a specific user, given its id"
   ([] (make-request "user" [] :GET))
   ([id] (make-request "users/%s" [id] :GET)))
 
+(defn followers
+  "Get the followers of the specified user"
+  [id & [options]]
+  (make-request "users/%s/followers" [id] :GET options))
+
 (defn following
+  "Get the followings of the specified user"
   [id & [options]]
   (make-request "users/%s/following" [id] :GET options))
 
 (defn user-cards
+  "Get the given user cards"
   [id & [options]]
   (make-request "users/%s/cards" [id] :GET options))
 
 (defn cards
+  "Get the authenticated user cards"
   [& [options]]
   (make-request "user/cards" [] :GET options))
 
 (defn card
+  "Get a specific card"
   [id]
   (make-request "cards/%s" [id] :GET))
 
 (defn create-card
+  "Create a card"
   [headline]
   (make-request "cards" [] :POST {:headline headline}))
 
 (defn micro
+  "Get a specific micro"
   [id]
   (make-request "micros/%s" [id] :GET options))
 
 (defn user-micros
+  "Get the authenticated user micros"
   [& [options]]
   (make-request "user/micros" [] :GET options))
 
 (defn micros
+  "Get a specific user micros"
   [id & [options]]
   (make-request "users/%s/micros" [id] :GET options))
 
 (defn create-micro
+  "Create a micro"
   [status]
   (make-request "micros" [] :POST {:status status}))
 
 (defn highfive
+  "Highfive an item (user, card ... etc)"
   [type id]
   (make-request "highfive" [] :POST {:type type :gfk id}))
